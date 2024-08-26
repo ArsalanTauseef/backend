0e86daf3708ced228780b01b975daab7bc24f508
ArsalanTauseef <142938248+ArsalanTauseef@users.noreply.github.com>
2024-08-26 06:58:09 +0500

    new updated vercel
    new updated vercel

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
-const port = 4000;
 const {
   getData,
   addContact,
   updateContacts,
   deleteContact,
 } = require("./functions/functions");
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 var cors = require("cors");
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// // Remove app.listen
+// module.exports = app;
