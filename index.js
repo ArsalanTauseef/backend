e2f183267eb7baf34b409b70775cc22b4b2807f8
ArsalanTauseef <142938248+ArsalanTauseef@users.noreply.github.com>
2024-08-26 07:20:13 +0500

    1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const {getData, addContact, updateContacts, deleteContact} = require('./funcs/funcs')
 app.use(express.json())
+
 
 app.get("/api/my-products", (req, res) => {
   const products = getData();
