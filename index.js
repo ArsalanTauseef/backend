74e31140f89690f8aac6fcde0132a8fc41228f3b
ArsalanTauseef <142938248+ArsalanTauseef@users.noreply.github.com>
2024-08-26 07:14:10 +0500

    adw

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-var bodyParser = require("body-parser");
-var cors = require("cors");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(cors());
 
 app.get("/api/my-products", (req, res) => {
   const products = getData();
