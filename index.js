5c35e81b42179d1bbd414bf213471809acce4599
ArsalanTauseef <142938248+ArsalanTauseef@users.noreply.github.com>
2024-08-26 06:50:49 +0500

    new 1
    new 1

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 4000;
+const {
+  getData,
+  addContact,
+  updateContacts,
+  deleteContact,
+} = require("./functions/functions");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+var cors = require("cors");
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
